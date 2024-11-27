1544cb2ec6a52d47aec1fcbdca844a9d6fb3660d
regand75 <regandess@gmail.com>
2024-11-27 19:06:16 +0300

    adding other pages to thw router

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -21,6 +21,96 @@ export class Router {
                     new Form();
                 }
             },
+            {
+                route: '#/sign-up',
+                title: 'Регистрация',
+                template: 'src/templates/pages/auth/sign-up.html',
+                styles: 'src/styles/index.css',
+                load: () => {
+                    new Form();
+                }
+            },
+            {
+                route: '#/incomes-expenses',
+                title: 'Доходы и расходы',
+                template: 'src/templates/pages/incomes-expenses/incomes-expenses.html',
+                styles: 'src/styles/index.css',
+                load: () => {
+
+                }
+            },
+            {
+                route: '#/incomes-expenses/edit',
+                title: 'Доходы и расходы',
+                template: 'src/templates/pages/incomes-expenses/edit.html',
+                styles: 'src/styles/index.css',
+                load: () => {
+
+                }
+            },
+            {
+                route: '#/incomes-expenses/creating',
+                title: 'Доходы и расходы',
+                template: 'src/templates/pages/incomes-expenses/creating.html',
+                styles: 'src/styles/index.css',
+                load: () => {
+
+                }
+            },
+            {
+                route: '#/incomes',
+                title: 'Доходы',
+                template: 'src/templates/pages/incomes/incomes.html',
+                styles: 'src/styles/index.css',
+                load: () => {
+
+                }
+            },
+            {
+                route: '#/incomes/edit',
+                title: 'Редактирование доходов',
+                template: 'src/templates/pages/incomes/edit.html',
+                styles: 'src/styles/index.css',
+                load: () => {
+
+                }
+            },
+            {
+                route: '#/incomes/creating',
+                title: 'Создание доходов',
+                template: 'src/templates/pages/incomes/creating.html',
+                styles: 'src/styles/index.css',
+                load: () => {
+
+                }
+            },
+            {
+                route: '#/expenses',
+                title: 'Расходы',
+                template: 'src/templates/pages/expenses/expenses.html',
+                styles: 'src/styles/index.css',
+                load: () => {
+
+                }
+            },
+            {
+                route: '#/expenses/edit',
+                title: 'Редактирование расходов',
+                template: 'src/templates/pages/expenses/edit.html',
+                styles: 'src/styles/index.css',
+                load: () => {
+
+                }
+            },
+            {
+                route: '#/expenses/creating',
+                title: 'Создание расходов',
+                template: 'src/templates/pages/expenses/creating.html',
+                styles: 'src/styles/index.css',
+                load: () => {
+
+                }
+            },
         ]
     }
 
