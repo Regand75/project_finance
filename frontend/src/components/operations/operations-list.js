e6562f2ff2557a786df1f5728efaa52583181ea0
Regand75 <regandess@gmail.com>
2024-12-09 13:57:07 +0300

    functional page operations

--- frontend/src/components/operations/operations-list.js
+++ frontend/src/components/operations/operations-list.js
@@ -1,25 +1,67 @@
 import {ModalManager} from "../modal.js";
 import {OperationsService} from "../../services/operations-service.js";
+import {OperationsUtils} from "../../utils/operations-utils.js";
 
 export class OperationsList {
     constructor() {
         this.buttonNoDeleteElement = document.getElementById('no-delete');
         this.recordsElement = document.getElementById('records');
+        this.filtersContainer = document.getElementById('filters-container'); // Родительский контейнер кнопок фильтра
 
-        if (this.recordsElement) {
-            this.recordsElement.addEventListener('click', this.handleDeleteClick.bind(this));
+        // this.buttonOperationsTodayElement = document.getElementById('operations-button-today');
+        // this.buttonOperationsWeekElement = document.getElementById('operations-button-week');
+        // this.buttonOperationsMouthElement = document.getElementById('operations-button-mouth');
+        // this.buttonOperationsYearElement = document.getElementById('operations-button-year');
+        // this.buttonOperationsAllElement = document.getElementById('operations-button-all');
+        // this.buttonOperationsintervalElement = document.getElementById('operations-button-interval');
+
+        // if (this.recordsElement) {
+        //     this.recordsElement.addEventListener('click', this.handleDeleteClick.bind(this));
+        // }
+
+        if (this.filtersContainer) {
+            this.filtersContainer.addEventListener('click', this.handleFilterClick.bind(this));
         }
 
         if (this.buttonNoDeleteElement) {
             this.buttonNoDeleteElement.addEventListener('click', ModalManager.hideModal);
         }
-        this.getOperations().then();
+        this.getOperations('all').then();
     }
 
-    async getOperations() {
-        const operationsResult = await OperationsService.getOperations('?period=all');
-        this.showRecords(operationsResult);
+    async getOperations(period) {
+        try {
+            const operationsResult = await OperationsService.getOperations(`?period=${period}`);
+            this.showRecords(operationsResult);
+        } catch (error) {
+            console.error('Ошибка при получении операций:', error);
+        }
     }
+    // async getOperations() {
+    //     const operationsResult = await OperationsService.getOperations('?period=all');
+    //     this.showRecords(operationsResult);
+    // }
+
+    async handleFilterClick(event) {
+        const target = event.target.closest('button[data-period]');
+        if (target) {
+            const period = target.getAttribute('data-period'); // Получаем значение фильтра
+            console.log(`Фильтр выбран: ${period}`);
+
+            try {
+                // Используем статический метод для получения операций
+                const operationsResult = await OperationsService.getOperations(`?period=${period}`);
+
+                // Передаем данные для отображения записей
+                this.showRecords(operationsResult);
+            } catch (error) {
+                console.error('Ошибка при получении операций:', error);
+                alert('Не удалось загрузить данные. Попробуйте позже.');
+            }
+        }
+    }
+
+
 
     showRecords(operations) {
         const recordsElement = this.recordsElement;
@@ -31,58 +73,52 @@ export class OperationsList {
         });
     }
 
-    // Функция для создания строки таблицы
+    // Создание строки таблицы
     createRow(operation, orderNumber) {
         const tr = document.createElement('tr');
 
         // Заменяем тип операции на русский язык и задаем цвет текста
         const operationType = operation.type === 'income' ? 'доход' : 'расход';
         const textClass = operation.type === 'income' ? 'text-success' : 'text-danger';
+
+        // Преобразуем дату в формат день.месяц.год
+        const date = new Date(operation.date); // Преобразуем строку в объект Date
+        const formattedDate = new Intl.DateTimeFormat('ru-RU').format(date);
+
+        // Форматируем сумму с разделением тысяч пробелом
+        const formattedAmount = Number(operation.amount).toLocaleString('ru-RU');
+
         tr.setAttribute('data-id', operation.id);
 
         tr.innerHTML = `
         <th scope="row">${orderNumber}</th>
         <td class="${textClass}">${operationType}</td>
         <td>${operation.category}</td>
-        <td>${operation.amount}</td>
-        <td>${operation.date}</td>
+        <td>${formattedAmount} $</td>
+        <td>${formattedDate}</td>
         <td>${operation.comment}</td>
         <td class="text-nowrap">
-            <a href="javascript:void(0)" class="operations-delete">
-                <!-- Иконка удаления -->
-                <svg class="me-2" width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4.5 5.5C4.77614 5.5 5 5.72386 5 6V12C5 12.2761 4.77614 12.5 4.5 12.5C4.22386 12.5 4 12.2761 4 12V6C4 5.72386 4.22386 5.5 4.5 5.5Z" fill="black"/>
-                    <path d="M7 5.5C7.27614 5.5 7.5 5.72386 7.5 6V12C7.5 12.2761 7.27614 12.5 7 12.5C6.72386 12.5 6.5 12.2761 6.5 12V6C6.5 5.72386 6.72386 5.5 7 5.5Z" fill="black"/>
-                    <path d="M10 6C10 5.72386 9.77614 5.5 9.5 5.5C9.22386 5.5 9 5.72386 9 6V12C9 12.2761 9.22386 12.5 9.5 12.5C9.77614 12.5 10 12.2761 10 12V6Z" fill="black"/>
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M13.5 3C13.5 3.55228 13.0523 4 12.5 4H12V13C12 14.1046 11.1046 15 10 15H4C2.89543 15 2 14.1046 2 13V4H1.5C0.947715 4 0.5 3.55228 0.5 3V2C0.5 1.44772 0.947715 1 1.5 1H5C5 0.447715 5.44772 0 6 0H8C8.55229 0 9 0.447715 9 1H12.5C13.0523 1 13.5 1.44772 13.5 2V3ZM3.11803 4L3 4.05902V13C3 13.5523 3.44772 14 4 14H10C10.5523 14 11 13.5523 11 13V4.05902L10.882 4H3.11803ZM1.5 3V2H12.5V3H1.5Z" fill="black"/>
-                </svg>
-            </a>
-            <a href="#/operations/edit" class="operations-edit">
-                <!-- Иконка редактирования -->
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12.1465 0.146447C12.3417 -0.0488155 12.6583 -0.0488155 12.8536 0.146447L15.8536 3.14645C16.0488 3.34171 16.0488 3.65829 15.8536 3.85355L5.85357 13.8536C5.80569 13.9014 5.74858 13.9391 5.68571 13.9642L0.68571 15.9642C0.500001 16.0385 0.287892 15.995 0.146461 15.8536C0.00502989 15.7121 -0.0385071 15.5 0.0357762 15.3143L2.03578 10.3143C2.06092 10.2514 2.09858 10.1943 2.14646 10.1464L12.1465 0.146447ZM11.2071 2.5L13.5 4.79289L14.7929 3.5L12.5 1.20711L11.2071 2.5ZM12.7929 5.5L10.5 3.20711L4.00001 9.70711V10H4.50001C4.77616 10 5.00001 10.2239 5.00001 10.5V11H5.50001C5.77616 11 6.00001 11.2239 6.00001 11.5V12H6.29291L12.7929 5.5ZM3.03167 10.6755L2.92614 10.781L1.39754 14.6025L5.21903 13.0739L5.32456 12.9683C5.13496 12.8973 5.00001 12.7144 5.00001 12.5V12H4.50001C4.22387 12 4.00001 11.7761 4.00001 11.5V11H3.50001C3.28561 11 3.10272 10.865 3.03167 10.6755Z" fill="black"/>
-                </svg>
-            </a>
+            ${OperationsUtils.generateGridToolsColumn('operations', operation.id)}
         </td>
     `;
         return tr;
     }
 
-    // Обработчик клика по кнопке удаления
-    handleDeleteClick(event) {
-        const target = event.target.closest('.operations-delete, .operations-edit');
-        if (target) {
-            const row = target.closest('tr'); // Находим родительскую строку
-            const operationId = row.getAttribute('data-id'); // Получаем id операции
-            if (target.classList.contains('operations-delete')) {
-                console.log(`Удаление операции с id: ${operationId}`);
-                ModalManager.showModal();
-            }
-
-            if (target.classList.contains('operations-edit')) {
-                console.log(`Редактирование операции с id: ${operationId}`);
-                // Логика для редактирования операции
-            }
-        }
-    }
+    // Обработчик клика по кнопке удаления и редактирования
+    // handleDeleteClick(event) {
+    //     const target = event.target.closest('.operations-delete, .operations-edit');
+    //     if (target) {
+    //         const row = target.closest('tr'); // Находим родительскую строку
+    //         const operationId = row.getAttribute('data-id'); // Получаем id операции
+    //         if (target.classList.contains('operations-delete')) {
+    //             console.log(`Удаление операции с id: ${operationId}`);
+    //             ModalManager.showModal();
+    //         }
+    //
+    //         if (target.classList.contains('operations-edit')) {
+    //             console.log(`Редактирование операции с id: ${operationId}`);
+    //             // Логика для редактирования операции
+    //         }
+    //     }
+    // }
 }
